ff6e731975745e69b5dc7612a859efa916c41b3b
Abdellah IDRISSI AZAMI <idrissi.azami.abdellah@gmail.com>
2022-07-17 03:59:31 +0100

    change

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import NavBar from './widgets/navbar';
+import "./App.css";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Home } from "./components/Pages/Home";
+import { About } from "./components/Pages/About";
+import { Blog } from "./components/Pages/Blog";
+import { Contact } from "./components/Pages/Contact";
 
 function App() {
   return (
-    <React.Fragment>
-      <NavBar></NavBar>
-    </React.Fragment>
+    <>
+      <Router>
+        <NavBar />
+
+        <div className="pages">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/blog" component={Blog} />
+            <Route path="/contact" component={Contact} />
+          </Switch>
+        </div>
+      </Router>
+    </>
   );
 }
 
