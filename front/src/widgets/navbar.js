458bf14e0a1a67e899ff2ff629a07e4eb430b3fc
Abdellah IDRISSI AZAMI <idrissi.azami.abdellah@gmail.com>
2022-07-17 02:13:57 +0100

    responsive navbar

--- front/src/widgets/navbar.js
+++ front/src/widgets/navbar.js
@@ -1,16 +1,23 @@
 import { FaBars, FaTimes} from 'react-icons/fa';
+import { useRef } from 'react';
+import  '../styles/nav.css'
 export default function NavBar() {
+    const navRef = useRef()
+
+    const showNavBar = () => {
+        navRef.current.classList.toggle('responsive_nav')
+    }
 return(
     <header>
         <h3><a href="#">Logo</a></h3>
-        <nav>
+        <nav ref={navRef}>
         <a href="#">Contact Us</a>
             <a href="#">Login</a>
-            <button>
+            <button className = 'nav-btn nav-close-btn' onClick={showNavBar}>
                 <FaTimes/>
             </button>
         </nav>
-        <button>
+        <button  className= 'nav-btn nav-show-btn' onClick={showNavBar}>
             <FaBars/>
         </button>
     </header>
