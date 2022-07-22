a750efdf16d5241c6492f0306cb4e64a7ed26a57
abdellahai <idrissi.azami.abdellah@gmail.com>
2022-07-22 21:09:27 +0100

    change

--- front/src/index.js
+++ front/src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import NavBar from './components/NavBar';
 import HomeView from './views/Home';
+import './styles/index.css'
 import { BrowserRouter , Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
