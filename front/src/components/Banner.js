15e6d7cdd0223c5858fe0d48a40c04f23ad83d50
abdellahai <idrissi.azami.abdellah@gmail.com>
2022-07-22 16:34:07 +0100

    change

--- front/src/components/Banner.js
+++ front/src/components/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                             {'Bonjour a la Platforme'}<span className='wrap'>CovMan</span>
                         </h1>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent elementum sagittis erat eu gravida. Pellentesque eget libero pulvinar, commodo nulla at, rutrum mi. Donec fermentum turpis in elit scelerisque, vitae dapibus orci pulvinar. Mauris laoreet, nibh imperdiet porttitor feugiat, ex dui finibus orci, id pulvinar velit lectus sit amet est. Vestibulum finibus dignissim diam. Vivamus pulvinar est in ex tincidunt dapibus. Suspendisse sit amet odio turpis.</p>
-                        <Button href='/register'>Commencer a utiliser <ArrowRightCircle size = {25}/></Button>                    
+                        <Button variant='outline-primary' href='/register'>Commencer a utiliser <ArrowRightCircle size = {25}/></Button>                    
                     </Col>
                     <Col xs={12} md={6} xl={7}>
 
