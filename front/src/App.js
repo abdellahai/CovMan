8b28959bc1c462cd7399f45fe6fbe180434e6c96
abdellahai <idrissi.azami.abdellah@gmail.com>
2022-07-22 15:24:54 +0100

    changes

--- front/src/App.js
+++ front/src/App.js
@@ -1,32 +1,13 @@
-import Layout from "./components/Layout";
-import { Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import PageOne from "./pages/contactus";
-import PageTwo from "./pages/about";
-import Login from "./pages/login";
-import Register from "./pages/register";
+
+
+import NavBar from './components/NavBar';
+
 
 function App() {
     return (
-        <Layout>
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/contactus">
-                    <PageOne />
-                </Route>
-                <Route path="/about">
-                    <PageTwo />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/register">
-                    <Register />
-                </Route>
-            </Switch>
-        </Layout>
+       <>
+       <NavBar></NavBar>
+       </>
     );
 }
 
---
+++ front/src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {Navbar, Nav, Container, Button} from 'react-bootstrap'
+const NavBar = ()=>{
+    return (
+        <Navbar expand='lg'>
+            <Container>
+                <Navbar.Brand href='/'>CovMan</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"></Navbar.Toggle>
+                <Navbar.Collapse id = 'basic-navbar-nav'>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/about">A propos</Nav.Link>
+                        <Nav.Link href='/contacus'>Contactez-nous</Nav.Link>
+                    </Nav><Nav> 
+                        <Nav.Link href='/login'>Se connecter</Nav.Link>
+                        <Button variant="outline-primary" href='/register'>S'inscrire</Button>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default NavBar;
